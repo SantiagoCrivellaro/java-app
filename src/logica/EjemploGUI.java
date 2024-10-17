e44b022e851b23fdb3410c873fd4beb30d5761a8
Santiago Crivellaro <crivellarosantiago1@gmail.com>
2024-10-17 08:29:40 -0300

    Innitial Commit

---
+++ src/logica/EjemploGUI.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+
+/**
+ *
+ * @i
+ */
+
+import GUI.Main;
+
+
+public class EjemploGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main ventana = new Main();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+    
+}
